97ef5dccc1a648cb987ad43983dd914256cc881a
Chiara De Sio <cdesio@sa.infn.it>
2019-02-21 15:03:00 +0100

    script simplified: data preparation removed. data loaded from npz files

--- run_UNet_50epochs_ih.py
+++ run_UNet_50epochs_ih.py
@@ -6,25 +6,23 @@ COL_SLICE = slice(1000, None)
 DATA_DIR_IH="/data/uob"
 DATA_DIR_DEEPTHOUGHT="/storage/yw18581/data"
 
-
 import os
 import numpy as np
-from UNet_inverted_dice import get_unet
-import tensorflow as tf
-
+from UNet import get_unet
+import tensorflow as tf 
 tf.keras.backend.clear_session()
 
 ## Loading data
 print("Loading data")
-data_dir = DATA_DIR_DEEPTHOUGHT
+
+data_dir = DATA_DIR_IH
 
 X_train = np.load(os.path.join(data_dir,"Jordan","Xy_train.npz"))["x"]
 y_train = np.load(os.path.join(data_dir,"Jordan","Xy_train.npz"))["y"]
-
 model = get_unet()
 model.summary()
 
-model.fit(x=X_train, y=y_train, epochs=100, batch_size=2, verbose=1, validation_split=.2)
+model.fit(x=X_train, y=y_train, epochs=50, batch_size=1, verbose=1, validation_split=.2)
 
-model.save(os.path.join(data_dir,"trained_UNet_100epochs_inv_dice.hdf5")
-sys.exit()
+model.save(os.path.join(data_dir,"trained_models","trained_UNet_50epochs.hdf5"))
+

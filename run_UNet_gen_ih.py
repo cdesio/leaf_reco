3b7f52f2bc3c2aba44da10b869dd1ee20822ec0e
Chiara De Sio <cdesio@sa.infn.it>
2019-03-01 16:35:28 +0100

    now works on ih

--- run_UNet_gen_ih.py
+++ run_UNet_gen_ih.py
@@ -6,6 +6,7 @@ COL_SLICE = slice(1000, None)
 import os
 from UNet import get_unet
 from data_loaders_km3 import data_generator, get_n_iterations
+from network_models_km3 import train_neural_network
 from os import path as p
 import tensorflow as tf
 
@@ -22,7 +23,12 @@ TRAIN_VAL_TEST_DIR = os.path.join(data_folder,"train_validation_test")
 N_FILES = 1
 BATCH_SIZE=3
 N_EPOCHS = 500
-CHECKPOINT_FOLDER_PATH = p.join(p.abspath(p.curdir), 'model')
+
+model = get_unet()
+model.summary()
+
+
+CHECKPOINT_FOLDER_PATH = p.join(data_folder, 'trained_models')
 TASK_NAME = 'UNet_training_generator_{}epochs'.format(N_EPOCHS)
 TASK_FOLDER_PATH = os.path.join(CHECKPOINT_FOLDER_PATH, TASK_NAME)
 
@@ -39,29 +45,25 @@ MODEL_JSON_FILEPATH = os.path.join(TASK_FOLDER_PATH, '{}.json'.format(model.name
 
 
 
-fname_train = os.path.join(TRAIN_VAL_TEST_DIR,"Xy_train.npz")
-fname_val = os.path.join(TRAIN_VAL_TEST_DIR,"Xy_val.npz")
+fname_train = [os.path.join(TRAIN_VAL_TEST_DIR,"Xy_train.npz")]
+fname_val = [os.path.join(TRAIN_VAL_TEST_DIR,"Xy_val.npz")]
 
 steps_per_epoch, n_events = get_n_iterations(fname_train, batch_size=BATCH_SIZE)
-print(steps_per_epoch, n_events)
+print("training steps per epoc:{}, number of events:{}".format(steps_per_epoch, n_events))
 
-#validation_steps, n_evts_val = get_n_iterations(fnames_val[:N_FILES], batch_size=BATCH_SIZE)
-#print(validation_steps, n_evts_val)
+validation_steps, n_evts_val = get_n_iterations(fname_val, batch_size=BATCH_SIZE)
+print("validation steps per epoch:{}, number of events:{}".format(validation_steps, n_evts_val))
 
-prediction_steps, n_evts_test = get_n_iterations(fname_test, batch_size=BATCH_SIZE)
-print(prediction_steps, n_evts_test)
-validation_steps, n_evts_val = get_n_iterations(fname_test, batch_size=BATCH_SIZE)
-print(validation_steps, n_evts_val)
+#prediction_steps, n_evts_test = get_n_iterations(fname_test, batch_size=BATCH_SIZE)
+#print(prediction_steps, n_evts_test)
 
 training_generator = data_generator(fname_train, batch_size=BATCH_SIZE,
                                     ftarget=lambda y: y)
 
 validation_generator = data_generator(fname_val, batch_size=BATCH_SIZE,
-                                      fdata=get_Time_Coord, ftarget=process_cosz)
+                                     ftarget=lambda y: y)
 #data_dir = DATA_DIR_IH
 
-model = get_unet()
-model.summary()
 
 training_history = train_neural_network(model, training_generator, steps_per_epoch,
                                         validation_generator,

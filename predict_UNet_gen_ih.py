90da1825a72c4a6d0a6b890dff914edce941a04a
Chiara De Sio <cdesio@sa.infn.it>
2019-03-01 16:11:51 +0000

    script to run prediction with data generators and save pred to output file

---
+++ predict_UNet_gen_ih.py
@@ -0,0 +1,82 @@
+IMG_WIDTH = 1400
+IMG_HEIGHT = 1400
+ROW_SLICE = slice(0, 1400)
+COL_SLICE = slice(1000, None)
+
+import os
+from UNet import get_unet
+from data_loaders_km3 import data_generator, get_n_iterations
+from os import path as p
+import tensorflow as tf
+
+
+tf.keras.backend.clear_session()
+
+DATA_DIR_IH="/data/uob"
+DATA_DIR_DEEPTHOUGHT="/storage/yw18581/data"
+
+data_folder = DATA_DIR_IH
+TRAIN_VAL_TEST_DIR = os.path.join(data_folder,"train_validation_test")
+
+N_FILES = 1
+BATCH_SIZE=3
+N_EPOCHS = 500
+
+model = get_unet()
+model.summary()
+
+
+CHECKPOINT_FOLDER_PATH = p.join(data_folder, 'trained_models')
+TASK_NAME = 'UNet_training_generator_{}epochs'.format(N_EPOCHS)
+TASK_FOLDER_PATH = os.path.join(CHECKPOINT_FOLDER_PATH, TASK_NAME)
+
+if not os.path.exists(TASK_FOLDER_PATH):
+    os.makedirs(TASK_FOLDER_PATH)
+
+#TRAINING_WEIGHTS_FILEPATH = os.path.join(TASK_FOLDER_PATH,
+#                                         '{}_weights_training{}.hdf5'.format(model.name, TASK_NAME))
+
+TRAINING_WEIGHTS_FILEPATH=os.path.join(CHECKPOINT_FOLDER_PATH,'retrained_UNet_500+250epochs.hdf5')
+
+fname_test = [os.path.join(TRAIN_VAL_TEST_DIR,"Xy_test.npz")]
+
+model.load_weights(TRAINING_WEIGHTS_FILEPATH)
+prediction_steps, n_evts_test = get_n_iterations(fname_test, batch_size=BATCH_SIZE)
+print(prediction_steps, n_evts_test)
+
+
+
+print('Saving Model (JSON), Training History & Weights...', end='')
+model_json_str = model.to_json()
+with open(MODEL_JSON_FILEPATH, 'w') as model_json_f:
+    model_json_f.write(model_json_str)
+
+history_filepath = HISTORY_FILEPATH
+dump(training_history.history, open(history_filepath, 'wb'))
+
+model.save_weights(TRAINING_WEIGHTS_FILEPATH)
+print('...Done!')
+
+
+print('INFERENCE STEP')
+
+test_data_gen = data_generator(fname_test, batch_size=BATCH_SIZE,
+                               ftarget=lambda y: y)
+
+def inference_step(network_model, test_data_generator, predict_steps):
+
+    y_pred = list()
+
+    for _ in tqdm(range(predict_steps)):
+        X_batch, _ = next(test_data_generator)
+        Y_batch_pred = network_model.predict_on_batch(X_batch)
+        y_pred.append(Y_batch_pred.ravel())
+    y_pred = np.hstack(np.asarray(y_pred))
+
+    return y_pred
+
+
+y_pred = inference_step(model, test_data_gen, prediction_steps)
+
+np.savez_compressed(os.path.join(TASK_FOLDER_PATH,"Xy_pred.npz"),
+                            x=X_test, y=y_pred)

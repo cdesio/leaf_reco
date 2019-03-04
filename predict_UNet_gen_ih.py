11cc3349ed86d484f604a68bc82c73c53cf36a4a
Chiara De Sio <cdesio@sa.infn.it>
2019-03-04 13:32:13 +0100

    tested on cpu

--- predict_UNet_gen_ih.py
+++ predict_UNet_gen_ih.py
@@ -20,11 +20,11 @@ data_folder = DATA_DIR_IH
 TRAIN_VAL_TEST_DIR = os.path.join(data_folder,"train_validation_test")
 
 N_FILES = 1
-BATCH_SIZE=3
+BATCH_SIZE=15
 N_EPOCHS = 500
 
-model = get_unet()
-model.summary()
+#model = get_unet()
+#model.summary()
 
 
 CHECKPOINT_FOLDER_PATH = p.join(data_folder, 'trained_models')
@@ -41,7 +41,7 @@ TRAINING_WEIGHTS_FILEPATH=os.path.join(CHECKPOINT_FOLDER_PATH,'retrained_UNet_50
 
 fname_test = [os.path.join(TRAIN_VAL_TEST_DIR,"Xy_test.npz")]
 
-model.load_weights(TRAINING_WEIGHTS_FILEPATH)
+#model.load_weights(TRAINING_WEIGHTS_FILEPATH)
 prediction_steps, n_evts_test = get_n_iterations(fname_test, batch_size=BATCH_SIZE)
 print("prediction steps per epoch:{}, n events:{}".format(prediction_steps, n_evts_test))
 
@@ -56,13 +56,23 @@ def inference_step(network_model, test_data_generator, predict_steps):
 
     for _ in tqdm(range(predict_steps)):
         X_batch, _ = next(test_data_generator)
-        Y_batch_pred = network_model.predict_on_batch(X_batch)
+        with tf.device('/device:CPU:0'):
+            Y_batch_pred = network_model.predict_on_batch(X_batch)
         y_pred.append(Y_batch_pred)
     y_pred = np.concatenate(y_pred, axis=0)
 
     return y_pred
 
-y_pred = inference_step(model, test_data_gen, prediction_steps)
+with tf.device('/device:CPU:0'):
+    model = get_unet()
+    model.load_weights(TRAINING_WEIGHTS_FILEPATH)
+    y_pred = list()
+    for _ in tqdm(range(prediction_steps)):
+        X_batch, _ = next(test_data_gen)
+        Y_batch_pred = model.predict_on_batch(X_batch)
+        y_pred.append(Y_batch_pred)
+    y_pred = np.concatenate(y_pred, axis=0)
+    #y_pred = inference_step(model, test_data_gen, prediction_steps)
 
-np.savez_compressed(os.path.join(TASK_FOLDER_PATH,"Xy_pred.npz"),
+np.savez_compressed(os.path.join(TASK_FOLDER_PATH,"TEST.npz"),
                             x=np.load(fname_test[0])['x'] , y=y_pred)

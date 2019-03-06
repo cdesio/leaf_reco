5eced62009cc5e13189c4098ac3fbef4bf65c246
Chiara De Sio <cdesio@sa.infn.it>
2019-03-06 10:27:06 +0000

    simplified ohe function

--- leaf_classifier.py
+++ leaf_classifier.py
@@ -56,11 +56,9 @@ print("validation steps per epoch:{}, number of events:{}".format(validation_ste
 
 def ohe(values):
 
-    label_encoder = LabelEncoder()
-    integer_encoded = label_encoder.fit_transform(values)
+    values_reshaped = values.reshape(-1,1)
     onehot_encoder = OneHotEncoder(sparse=False)
-    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    onehot_encoded = onehot_encoder.fit_transform(values_reshaped)
     return onehot_encoded
 
 

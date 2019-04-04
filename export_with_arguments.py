5b625581d5d92818f7aafdbadc0e37725e778e38
Dr Chiara De Sio <yw18581@soolin.dice.priv>
2019-04-04 12:41:11 +0100

    fixed typo

--- export_with_arguments.py
+++ export_with_arguments.py
@@ -6,9 +6,11 @@ COL_SLICE = slice(1000, None)
 import os
 import sys
 import re
+import numpy as np
+from matplotlib.image import imread
 regex = re.compile(r'\d+')
 
-if not len(sys.argv)==2:
+if not len(sys.argv)==3:
     raise Exception("Please insert folder name and out_str")
 
 DATA_DIR_IH="/data/uob"
@@ -16,8 +18,6 @@ DATA_DIR_DEEPTHOUGHT="/storage/yw18581/data"
 data_dir = DATA_DIR_DEEPTHOUGHT
 
 source_data_folder = data_dir
-print(data_dir)
-print(source_data_folder)
 TRAIN_VAL_TEST_DIR = os.path.join(data_dir, "train_validation_test")
 
 
@@ -28,7 +28,7 @@ if not os.path.exists(TRAIN_VAL_TEST_DIR):
 ## Loading data
 print("Loading data filenames")
 folder = os.path.join(source_data_folder, sys.argv[1])
-
+print("read data from {}".format(folder))
 #folder_15mm = os.path.join(source_data_folder, "10x10_15mm_v2_8bit")
 
 from train_test_validation_metadata_dt import get_filename_and_data
@@ -45,6 +45,7 @@ y = np.asarray([imread(df_mask['path'].iloc[i])[ROW_SLICE, COL_SLICE] for i in r
 
 metadata = np.asarray(df_orig['dist'], dtype=int)
 
+print("data loaded")
 
 print(X.shape, y.shape, metadata.shape)
 

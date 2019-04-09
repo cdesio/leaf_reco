93664f3b1ef8ed6a880cf2e9211edef7a4491804
cdesio <chiara.desio@bristol.ac.uk>
2019-04-09 14:48:22 +0100

    typo

--- train_test_validation_metadata_arguments_dt.py
+++ train_test_validation_metadata_arguments_dt.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     #      len(fnames_mask_20mm), len(fnames_mask_30mm))
     #print(len(fnames_orig_2mm), len(fnames_orig_4mm), len(fnames_orig_10mm),
     #      len(fnames_orig_20mm), len(fnames_orig_30mm))
-
+    print(folders)
     df_orig = pd.concat([get_filename_and_data_image(os.path.join(source_data_folder, folder)) for folder in folders])
     df_mask = pd.concat([get_filename_and_data_mask(os.path.join(source_data_folder, folder)) for folder in folders])
 

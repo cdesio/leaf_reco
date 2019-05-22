056f47221b347cd7113a459270aa9e7c04b09baf
Chiara De Sio <chiara.desio@live.com>
2019-05-22 16:44:30 +0100

    now prints number of files in each dir, and files to process

--- run_Leaf_mask_inv_selection_dt.py
+++ run_Leaf_mask_inv_selection_dt.py
@@ -13,11 +13,16 @@ f_clean = [np.int(regex.findall(i)[0]) for i in os.listdir(out_folder) if "mask"
 f_tot = [np.int(regex.findall(i)[0]) for i in os.listdir(in_folder) if "mask" in i and str(i).startswith("File")]
 fbad = [nb for nb in f_tot if nb not in f_clean]
 print(f_clean, f_tot, fbad)
+print("files in clean dir:{}, files in original dir:{}, files to process:{}".format(len(f_clean), len(f_tot), len(fbad)))
+
 print(in_folder, dist, out_folder)
 for nb in sorted(fbad):
     #print(nb)
     os.system("python /storage/yw18581/src/leaf_reco/Leaf_mask_inv.py {} {} {} {}".format(in_folder, nb, dist, out_folder))
-
+print("masks created!")
+print("copy remaining input files to clean dir")
 for i in sorted(os.listdir(in_folder)):
     if "mask" not in i:
         os.system("cp {}/{} {}/".format(in_folder, i, out_folder))
+
+print("done")

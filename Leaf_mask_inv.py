a9630582f540876e8a270a8bb2bc8fb0cc5bb1f9
cdesio <chiara.desio@bristol.ac.uk>
2019-08-07 12:45:40 +0100

    testing new select_profile -faster and optimised

--- Leaf_mask_inv.py
+++ Leaf_mask_inv.py
@@ -36,8 +36,16 @@ def select_profile(array):
         #print(i,np.max(np.argwhere(array)[:,1][np.argwhere(array)[:,0]==i]))
     sel = np.asarray(sel)
     return sel
+def select_profile_2(array):
+    arr = np.argwhere(array)
+    y = arr[:,0]
+    X = arr[:,1]
+    y_uniques = np.unique(y)
+    y_out = [np.max(X[y==yi]) for yi in y_uniques]
+    return np.column_stack(y_uniques, y_out)
 
-leaf = select_profile(ls[1000:1280])
+#leaf = select_profile(ls[1000:1280])
+leaf = select_profile_2(ls[1000:1280])
 leaf_position = np.min(leaf[:,1])
 print("calculated leaf position:{}".format(leaf_position))
 

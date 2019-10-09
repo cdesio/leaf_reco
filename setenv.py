6910f7e71ba66dbd7c187fa11519462b2fae2195
cdesio <chiara.desio@bristol.ac.uk>
2019-10-09 12:21:58 +0100

    script to run sys.path.append for different folders

---
+++ setenv.py
@@ -0,0 +1,13 @@
+import sys, os
+
+def add_folders:
+    src_folder = os.path.join("/","storage", "yw18581", "src")
+
+    ml_models = os.path.join(src_folder, "ml_models")
+    data_prep = os.path.join(src_folder, "data_preparation")
+    utilities = os.path.join(src_folder, "utilities")
+
+    sys.path.append(ml_models)
+    sys.path.append(utilities)
+    sys.path.append(data_prep)
+    return

4467ce058b0ca5eea4feeff0ded9899b83fffa23
Dr Chiara De Sio <yw18581@soolin.dice.priv>
2019-10-11 10:23:43 +0100

    modified to run 2 positions

--- training_scripts/run_rUNet_training_6positions_variable_coefficients.py
+++ training_scripts/run_rUNet_training_6positions_variable_coefficients.py
@@ -13,9 +13,9 @@ DATA_DIR_DEEPTHOUGHT = "/storage/yw18581/data"
 data_dir = DATA_DIR_DEEPTHOUGHT
 src_dir = os.path.join("/","storage", "yw18581", "src", "leaf_reco")
 root_dir = os.path.join(data_dir, "dataset")
-excluded = exclude_dist(dist_list = [1,3,15,30], root_folder=root_dir)
-
-task_folder = "trained_6_positions"
+excluded = exclude_dist(dist_list = [1,2,3,4,10,15,20,30], root_folder=root_dir)
+print(excluded)
+task_folder = "trained_2_positions"
 
 print("Load dataset")
 data_loaders, data_lengths = define_dataset(root_folder=root_dir, batch_size=16, excluded_list = excluded)
@@ -38,8 +38,8 @@ for coef in coeffs:
                                task_folder_name= task_folder,
                                data_loaders=data_loaders, data_lengths=data_lengths,
                                epochs=n_epochs, batch_size=16, model_checkpoint=10,
-                               dataset_key="6positions", writer=True)
-    history_filepath = os.path.join(src_dir, "saved_models", task_folder, "history_6positions_{}epochs_{}coef.pkl".format(n_epochs, coef))
+                               dataset_key="2positions", writer=True)
+    history_filepath = os.path.join(src_dir, "saved_models", task_folder, "history_2positions_{}epochs_{}coef.pkl".format(n_epochs, coef))
     print("Save history to {}".format(history_filepath))
     dump(history, open(history_filepath, 'wb'))
     print("Done")

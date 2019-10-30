43d1aef99bd56cae9ae0aa9b4115ba0e0e0a81f7
cdesio <chiara.desio@bristol.ac.uk>
2019-10-30 14:00:21 +0000

    modified define_dataset function: it gets an alldata parameter to create a non-split dataset for test on all-data; modified inference accordingly

--- utilities/utility_functions.py
+++ utilities/utility_functions.py
@@ -170,7 +170,7 @@ def inference_phase_rUNet(model, data_loaders, data_lengths, batch_size, dev=0, 
     return y_true, y_pred
 
 
-def inference_phase_rUNet_plot_notebook(model, data_loaders, data_lengths, batch_size, stop = 1, dev=0):
+def inference_phase_rUNet_plot_notebook(model, data_loaders, data_lengths, batch_size, stop = 1, dev=0, test=True):
     from tqdm.notebook import tqdm
     import matplotlib.pyplot as plt
 
@@ -180,31 +180,56 @@ def inference_phase_rUNet_plot_notebook(model, data_loaders, data_lengths, batch
     #model.load_state_dict(torch.load(model_name))
     model.eval()
     model.to(device);
+    if test:
+        for i, batch in tqdm(enumerate(data_loaders["test"]), total=data_lengths["test"] // batch_size, desc="Batch"):
 
-    for i, batch in tqdm(enumerate(data_loaders["test"]), total=data_lengths["test"] // batch_size, desc="Batch"):
+            true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
+            pred_masks, pred_dists = model(true_images.float().to(device))
+            print("batch {}".format(i + 1))
+            for j, (img, tr_msk, tr_dist, pr_msk, pr_dist) in enumerate(zip(true_images,
+                                                                            true_masks,
+                                                                            true_dists.cpu().detach().numpy(),
+                                                                            pred_masks.cpu().detach().numpy(),
+                                                                            pred_dists.cpu().detach().numpy())):
 
-        true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
-        pred_masks, pred_dists = model(true_images.float().to(device))
-        print("batch {}".format(i + 1))
-        for j, (img, tr_msk, tr_dist, pr_msk, pr_dist) in enumerate(zip(true_images,
-                                                                        true_masks,
-                                                                        true_dists.cpu().detach().numpy(),
-                                                                        pred_masks.cpu().detach().numpy(),
-                                                                        pred_dists.cpu().detach().numpy())):
+                print("{}: true_dist: {}, pred_dist: {}".format(j + 1, tr_dist, pr_dist))
 
-            print("{}: true_dist: {}, pred_dist: {}".format(j + 1, tr_dist, pr_dist))
+                f = plt.figure(figsize=(10, 5))
+                f.add_subplot(1, 3, 1)
+                plt.imshow(img[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 2)
+                plt.imshow(tr_msk[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 3)
+                plt.imshow(pr_msk[0, ...], cmap='gray')
+                plt.show(block=True)
 
-            f = plt.figure(figsize=(10, 5))
-            f.add_subplot(1, 3, 1)
-            plt.imshow(img[0, ...], cmap='gray')
-            f.add_subplot(1, 3, 2)
-            plt.imshow(tr_msk[0, ...], cmap='gray')
-            f.add_subplot(1, 3, 3)
-            plt.imshow(pr_msk[0, ...], cmap='gray')
-            plt.show(block=True)
+            if i == stop:
+                break
+        else:
+            for i, batch in tqdm(enumerate(data_loaders), total=data_lengths // batch_size,
+                                 desc="Batch"):
 
-        if i == stop:
-            break
+                true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
+                pred_masks, pred_dists = model(true_images.float().to(device))
+                print("batch {}".format(i + 1))
+                for j, (img, tr_msk, tr_dist, pr_msk, pr_dist) in enumerate(zip(true_images,
+                                                                                true_masks,
+                                                                                true_dists.cpu().detach().numpy(),
+                                                                                pred_masks.cpu().detach().numpy(),
+                                                                                pred_dists.cpu().detach().numpy())):
+                    print("{}: true_dist: {}, pred_dist: {}".format(j + 1, tr_dist, pr_dist))
+
+                    f = plt.figure(figsize=(10, 5))
+                    f.add_subplot(1, 3, 1)
+                    plt.imshow(img[0, ...], cmap='gray')
+                    f.add_subplot(1, 3, 2)
+                    plt.imshow(tr_msk[0, ...], cmap='gray')
+                    f.add_subplot(1, 3, 3)
+                    plt.imshow(pr_msk[0, ...], cmap='gray')
+                    plt.show(block=True)
+
+                if i == stop:
+                    break
         return
 
 def splitter(dataset, validation_split=0.2, batch=16, workers=4):

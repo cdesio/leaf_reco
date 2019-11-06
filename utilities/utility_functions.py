2146eadc15e622eb285d79234cb14ef87679738a
Chiara De Sio <chiara.desio@live.com>
2019-11-06 17:48:53 +0000

    include in define dataset

--- utilities/utility_functions.py
+++ utilities/utility_functions.py
@@ -11,10 +11,11 @@ import torch.nn as nn
 from pickle import dump
 
 def define_dataset(root_folder, batch_size=16, validation_split = 0.2, test_split=0.2,
-                   excluded_list=None, scale=0.25, multi_processing=0, alldata=False):
+                   excluded_list=None, include_list = None, scale=0.25, multi_processing=0, alldata=False):
     excluded = excluded_list
+    include = include_list
     composed = transforms.Compose([Cut(), Rescale(scale), ChannelsFirst(), ToTensor()])
-    dataset = UNetDatasetFromFolders(root_folder, excluded = excluded, transform=composed)
+    dataset = UNetDatasetFromFolders(root_folder, excluded = excluded, include = include, transform=composed)
     if alldata:
         data_loaders = DataLoader(dataset, batch_size=batch_size, num_workers=multi_processing)
         data_lengths = len(dataset)

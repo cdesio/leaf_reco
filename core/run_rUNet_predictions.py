74bad5fe8eee04eb5dfe89824e815ea62bf23b71
Dr Chiara De Sio <yw18581@soolin.dice.priv>
2019-11-13 18:18:16 +0000

    ready to run on dt

--- core/run_rUNet_predictions.py
+++ core/run_rUNet_predictions.py
@@ -2,14 +2,16 @@ import os, sys
 from sklearn.metrics import mean_squared_error
 import re
 import numpy as np
-from .utils.data import define_dataset, select_dist
-from .utils.inference import inference_phase_rUNet
-from .models import rUNet
+from utils.data import define_dataset, select_dist
+from utils.inference import inference_phase_rUNet
+from models import rUNet
 import torch
 
 
-data_dir = sys.argv[1]
-saved_models = sys.argv[2]
+#data_dir = sys.argv[1]
+#saved_models = sys.argv[2]
+data_dir = '/storage/yw18581/data'
+saved_models = '/storage/yw18581/src/leaf_reco/saved_models/trained_6positions'
 
 distances_unseen = [2, 4, 10, 20, 25, 35]
 distances_testdata = [1, 3, 15, 30]
@@ -59,7 +61,7 @@ def mse_vs_epochs(coeff, key, data_loaders, data_lengths, only_test=True):
         checkpoint = torch.load(os.path.join(saved_models, fname))['model_state_dict'];
         model.load_state_dict(checkpoint)
         y_true, y_pred = inference_phase_rUNet(model, data_loaders, data_lengths, batch_size=16,
-                                               notebook=True, test=only_test)
+                                               notebook=False, test=only_test)
         np.savez_compressed(os.path.join(saved_models, '_'.join(['predicted',
                                                                  key,
                                                                  fname.split('_')[3],
@@ -73,11 +75,12 @@ def mse_vs_epochs(coeff, key, data_loaders, data_lengths, only_test=True):
 
 #coeff_list_test = [25, 3, 4, 5, 6, 7, 75]
 
-coeff_list_test = sys.argv[3]
-for c in coeff_list_test:
-    _, _ = mse_vs_epochs(c, 'testdata', data_loaders_test, data_lengths_test)
+#coeff_list_test = sys.argv[3]
+#for c in coeff_list_test:
+#    _, _ = mse_vs_epochs(c, 'testdata', data_loaders_test, data_lengths_test)
 
-#coeff_list_unseen = [25, 3, 4, 5, 6, 7, 75]
-coeff_list_unseen = sys.argv[4]
+coeff_list_unseen = [25, 3, 4]
+#coeff_list_unseen = sys.argv[4]
+
 for cu in coeff_list_unseen:
     _, _ = mse_vs_epochs(cu, 'unseen', data_loaders_unseen, data_lengths_unseen, only_test=False)

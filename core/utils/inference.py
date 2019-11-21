4373e883a4caed829e0971ea73203a8d80d9b5aa
cdesio <chiara.desio@bristol.ac.uk>
2019-11-21 10:34:39 +0000

    fixed indentation in inference notebook

--- core/utils/inference.py
+++ core/utils/inference.py
@@ -76,29 +76,29 @@ def inference_phase_rUNet_plot_notebook(model, data_loaders, data_lengths, batch
 
             if i == stop:
                 break
-        else:
-            for i, batch in tqdm(enumerate(data_loaders), total=data_lengths // batch_size,
-                                 desc="Batch"):
+    else:
+        for i, batch in tqdm(enumerate(data_loaders), total=data_lengths // batch_size,
+                             desc="Batch"):
 
-                true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
-                pred_masks, pred_dists = model(true_images.float().to(device))
-                print("batch {}".format(i + 1))
-                for j, (img, tr_msk, tr_dist, pr_msk, pr_dist) in enumerate(zip(true_images,
-                                                                                true_masks,
-                                                                                true_dists.cpu().detach().numpy(),
-                                                                                pred_masks.cpu().detach().numpy(),
-                                                                                pred_dists.cpu().detach().numpy())):
-                    print("{}: true_dist: {}, pred_dist: {}".format(j + 1, tr_dist, pr_dist))
+            true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
+            pred_masks, pred_dists = model(true_images.float().to(device))
+            print("batch {}".format(i + 1))
+            for j, (img, tr_msk, tr_dist, pr_msk, pr_dist) in enumerate(zip(true_images,
+                                                                            true_masks,
+                                                                            true_dists.cpu().detach().numpy(),
+                                                                            pred_masks.cpu().detach().numpy(),
+                                                                            pred_dists.cpu().detach().numpy())):
+                print("{}: true_dist: {}, pred_dist: {}".format(j + 1, tr_dist, pr_dist))
 
-                    f = plt.figure(figsize=(10, 5))
-                    f.add_subplot(1, 3, 1)
-                    plt.imshow(img[0, ...], cmap='gray')
-                    f.add_subplot(1, 3, 2)
-                    plt.imshow(tr_msk[0, ...], cmap='gray')
-                    f.add_subplot(1, 3, 3)
-                    plt.imshow(pr_msk[0, ...], cmap='gray')
-                    plt.show(block=True)
+                f = plt.figure(figsize=(10, 5))
+                f.add_subplot(1, 3, 1)
+                plt.imshow(img[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 2)
+                plt.imshow(tr_msk[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 3)
+                plt.imshow(pr_msk[0, ...], cmap='gray')
+                plt.show(block=True)
 
-                if i == stop:
-                    break
+            if i == stop:
+                break
         return

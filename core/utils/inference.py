44556e861a2f5b6f200dcbba2a2b4aed0da4af7e
cdesio <chiara.desio@bristol.ac.uk>
2019-12-06 11:28:58 +0000

    inference_phase for unet: no predicted distance

--- core/utils/inference.py
+++ core/utils/inference.py
@@ -102,3 +102,63 @@ def inference_phase_rUNet_plot_notebook(model, data_loaders, data_lengths, batch
             if i == stop:
                 break
         return
+
+def inference_phase_UNet_plot_notebook(model, data_loaders, data_lengths, batch_size, stop=1, dev=0, test=True):
+    from tqdm.notebook import tqdm
+    import matplotlib.pyplot as plt
+
+    device = torch.device("cuda:{}".format(dev) if torch.cuda.is_available() else "cpu")
+    # model = cUNet(out_size=1)
+
+    # model.load_state_dict(torch.load(model_name))
+    model.eval()
+    model.to(device);
+    if test:
+        for i, batch in tqdm(enumerate(data_loaders["test"]), total=data_lengths["test"] // batch_size,
+                             desc="Batch"):
+
+            true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
+            pred_masks = model(true_images.float().to(device))
+            print("batch {}".format(i + 1))
+            for j, (img, tr_msk, tr_dist, pr_msk) in enumerate(zip(true_images,
+                                                                            true_masks,
+                                                                            true_dists.cpu().detach().numpy(),
+                                                                            pred_masks.cpu().detach().numpy())):
+                print("{}: true_dist: {}".format(j + 1, tr_dist))
+
+                f = plt.figure(figsize=(10, 5))
+                f.add_subplot(1, 3, 1)
+                plt.imshow(img[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 2)
+                plt.imshow(tr_msk[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 3)
+                plt.imshow(pr_msk[0, ...], cmap='gray')
+                plt.show(block=True)
+
+            if i == stop:
+                break
+    else:
+        for i, batch in tqdm(enumerate(data_loaders), total=data_lengths // batch_size,
+                             desc="Batch"):
+
+            true_images, true_masks, true_dists = batch["image"], batch["mask"], batch["dist"]
+            pred_masks = model(true_images.float().to(device))
+            print("batch {}".format(i + 1))
+            for j, (img, tr_msk, tr_dist, pr_msk) in enumerate(zip(true_images,
+                                                                            true_masks,
+                                                                            true_dists.cpu().detach().numpy(),
+                                                                            pred_masks.cpu().detach().numpy())):
+                print("{}: true_dist: {}".format(j + 1, tr_dist))
+
+                f = plt.figure(figsize=(10, 5))
+                f.add_subplot(1, 3, 1)
+                plt.imshow(img[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 2)
+                plt.imshow(tr_msk[0, ...], cmap='gray')
+                f.add_subplot(1, 3, 3)
+                plt.imshow(pr_msk[0, ...], cmap='gray')
+                plt.show(block=True)
+
+            if i == stop:
+                break
+        return

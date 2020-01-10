9525b630e7ca1e9bcabfc428604ddff59cc49c68
Chiara De Sio <chiara.desio@live.com>
2020-01-10 12:23:06 +0000

    added cases without maks

--- core/utils/data/transformers.py
+++ core/utils/data/transformers.py
@@ -12,27 +12,33 @@ COL_SLICE = slice(1000, None)
 
 class ChannelsFirst:
     def __call__(self, sample):
-        if len(sample.keys()) == 3:
-            image, mask, dist = sample['image'], sample['mask'], sample['dist']
-        elif len(sample.keys()) == 2:
-            image, mask = sample['image'], sample['mask']
+        if 'image' in sample.keys():
+            image = sample['image']
+        if 'mask' in sample.keys():
+            mask = sample['mask']
+        if 'dist' in sample.keys():
+            dist = sample['dist']
 
         if len(image.shape) == 3:
             image_sw1 = image.swapaxes(2, 0)
             image_sw2 = image_sw1.swapaxes(2,1)
             image_out = image_sw2
-            mask_sw1 = mask.swapaxes(2, 0)
-            mask_sw2 = mask_sw1.swapaxes(2,1)
-            mask_out = mask_sw2
+            if mask:
+                mask_sw1 = mask.swapaxes(2, 0)
+                mask_sw2 = mask_sw1.swapaxes(2,1)
+                mask_out = mask_sw2
 
         elif len(image.shape) == 2:
             image_out = image[np.newaxis, ...]
-            mask_out = mask[np.newaxis, ...]
+            if mask:
+                mask_out = mask[np.newaxis, ...]
 
-        if len(sample.keys()) == 3:
+        if 'mask' in sample.keys() and 'dist' in sample.keys():
             sample_out = {'image': image_out, 'mask': mask_out, 'dist': dist}
-        elif len(sample.keys()) == 2:
+        elif 'dist' not in sample.keys() and 'mask' in sample.keys():
             sample_out = {'image': image_out, 'mask': mask_out}
+        elif 'dist' not in sample.keys() and 'mask' not in sample.keys():
+            sample_out = {'image': image_out}
         return sample_out
 
 
@@ -43,24 +49,30 @@ class Rescale:
         self.output_scale = scale
 
     def __call__(self, sample):
-        if len(sample.keys()) == 3:
-            image, mask, dist = sample['image'], sample['mask'], sample['dist']
-        elif len(sample.keys()) == 2:
-            image, mask = sample['image'], sample['mask']
+        if 'image' in sample.keys():
+            image = sample['image']
+        if 'mask' in sample.keys():
+            mask = sample['mask']
+        if 'dist' in sample.keys():
+            dist = sample['dist']
 
         if len(image.shape) == 3:
             resizer = partial(rescale, scale=self.output_scale, anti_aliasing=True, multichannel=True)
             out_image = resizer(image)
-            out_mask = resizer(mask)
+            if mask:
+                out_mask = resizer(mask)
         elif len(image.shape) == 2:
             resizer = partial(rescale, scale=self.output_scale, anti_aliasing=True, multichannel=False)
             out_image = resizer(image)
-            out_mask = resizer(mask)
+            if mask:
+                out_mask = resizer(mask)
 
-        if len(sample.keys()) == 3:
-            sample_out = {'image': out_image, 'mask': out_mask, 'dist': dist}
-        elif len(sample.keys()) == 2:
-            sample_out = {'image': out_image, 'mask': out_mask}
+        if 'mask' in sample.keys() and 'dist' in sample.keys():
+            sample_out = {'image': image_out, 'mask': mask_out, 'dist': dist}
+        elif 'dist' not in sample.keys() and 'mask' in sample.keys():
+            sample_out = {'image': image_out, 'mask': mask_out}
+        elif 'dist' not in sample.keys() and 'mask' not in sample.keys():
+            sample_out = {'image': image_out}
         return sample_out
 
 
@@ -79,6 +91,10 @@ class ToTensor:
             img_tensor = torch.from_numpy(image)
             mask_tensor = torch.from_numpy(mask)
             sample_out = {'image': img_tensor, 'mask': mask_tensor}
+        elif len(sample.keys()) == 1:
+            image = sample['image']
+            img_tensor = torch.from_numpy(image)
+            sample_out = {'image': img_tensor}
         return sample_out
 
 
@@ -93,13 +109,12 @@ class Cut:
         self.cut = cut
 
     def __call__(self, sample):
-        if len(sample.keys()) == 3:
-            image, mask, dist = sample['image'], sample['mask'], sample['dist']
-        elif len(sample.keys()) == 2:
-            image, mask = sample['image'], sample['mask']
-        elif len(sample.keys()) ==1:
+        if 'image' in sample.keys():
             image = sample['image']
-            mask = None
+        if 'mask' in sample.keys():
+            mask = sample['mask']
+        if 'dist' in sample.keys():
+            dist = sample['dist']
 
         if self.cut:
             out_image = image[ROW_SLICE, COL_SLICE]
@@ -110,10 +125,10 @@ class Cut:
             if mask:
                 out_mask = mask
 
-        if len(sample.keys()) == 3:
-            sample_out = {'image': out_image, 'mask': out_mask, 'dist': dist}
-        elif len(sample.keys()) == 2:
-            sample_out = {'image': out_image, 'mask': out_mask}
-        elif len(sample.keys()) == 1:
-            sample_out = {'image': out_image}
+        if 'mask' in sample.keys() and 'dist' in sample.keys():
+            sample_out = {'image': image_out, 'mask': mask_out, 'dist': dist}
+        elif 'dist' not in sample.keys() and 'mask' in sample.keys():
+            sample_out = {'image': image_out, 'mask': mask_out}
+        elif 'dist' not in sample.keys() and 'mask' not in sample.keys():
+            sample_out = {'image': image_out}
         return sample_out

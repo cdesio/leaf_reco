747624e9c15a91b9d732fff3bb9216743101f939
Chiara De Sio <chiara.desio@live.com>
2020-01-10 11:42:57 +0000

    added if self.load_mask

--- core/utils/data/dataset.py
+++ core/utils/data/dataset.py
@@ -38,11 +38,12 @@ class UNetDataSetFromNpz(Dataset):
 
 class UNetDatasetFromFolders(Dataset):
 
-    def __init__(self, root_path, fname_key, file_extension, excluded=None, included=None, transform=None):
+    def __init__(self, root_path, fname_key, file_extension, load_masks = True, excluded=None, included=None, transform=None):
         self.transform = transform
         self.root_path = root_path
         self.key = fname_key
         self.extension = file_extension
+        self.load_masks = load_masks
         self.excluded = excluded
         self.included = included
         self.distances, self.images_list, self.masks_list = self._create_list()
@@ -66,13 +67,15 @@ class UNetDatasetFromFolders(Dataset):
         regex = re.compile(r'\d+')
         distances = []
         images_list = []
-        masks_list = []
+        if self.load_masks:
+            masks_list = []
 
         for root_dir, _, files in os.walk(self.root_path):
             image_found = 0
             mask_found = 0
             folder_imgs = []
-            folder_masks = []
+            if self.load_masks:
+                folder_masks = []
             r_folder = os.path.split(root_dir)[1]
             if (self.included and r_folder not in self.included) or (self.excluded and r_folder in self.excluded):
                 continue
@@ -83,19 +86,23 @@ class UNetDatasetFromFolders(Dataset):
                         image_found += 1
                         folder_imgs.append(os.path.join(root_dir, fname))
                     elif 'mask' in fname:
-                        mask_found += 1
-                        folder_masks.append(os.path.join(root_dir, fname))
+                        if self.load_masks:
+                            mask_found += 1
+                            folder_masks.append(os.path.join(root_dir, fname))
+                        else:
+                            mask_found=0
 
-            if len(folder_masks)!=0:
+            if self.load_masks:
                 assert len(folder_imgs) == len(folder_masks)
                 assert image_found == mask_found
 
             folder_imgs = sorted(folder_imgs, key=self.file_sort_key)
-            folder_masks = sorted(folder_masks, key=self.file_sort_key)
+            if self.load_masks:
+                folder_masks = sorted(folder_masks, key=self.file_sort_key)
 
             images_list.extend(folder_imgs)
-            masks_list.extend(folder_masks)
-
+            if self.load_masks:
+                masks_list.extend(folder_masks)
             if image_found or mask_found:
                 folder = root_dir.split('/')[-1]
                 dist = regex.findall(folder.split('_')[1])[0]
@@ -105,10 +112,12 @@ class UNetDatasetFromFolders(Dataset):
 
     def __getitem__(self, idx):
         image = imread(self.images_list[idx])
-        mask = imread(self.masks_list[idx])
         distance = self.distances[idx]
-        sample = {'image': image, 'mask': mask, 'dist': distance}
-
+        if self.load_masks:
+            mask = imread(self.masks_list[idx])
+            sample = {'image': image, 'mask': mask, 'dist': distance}
+        else:
+            sample = {'image': image, 'dist': distance}
         if self.transform:
             sample = self.transform(sample)
         return sample

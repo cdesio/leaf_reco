249ae9b1642903c0bcda38f8cd83225db71f68df
Chiara De Sio <chiara.desio@live.com>
2020-02-19 14:05:53 +0000

    modified parameter add_noise with actual value of var

--- core/utils/data/__init__.py
+++ core/utils/data/__init__.py
@@ -14,12 +14,12 @@ COL_SLICE = slice(1000, None)
 ROW_SLICE = slice(1000, 2400)
 #ROW_SLICE = slice(0, 1400)
 def define_dataset(root_folder, fname_key='File', file_extension='.tiff', batch_size=16, validation_split=0.2, test_split=0.2,
-                   excluded_list=None, include_list=None, load_mask = True,  add_noise = False, scale=0.25, multi_processing=0, alldata=False,
+                   excluded_list=None, include_list=None, load_mask = True,  add_noise = 0, scale=0.25, multi_processing=0, alldata=False,
                    row_slice=ROW_SLICE, col_slice=COL_SLICE):
     excluded = excluded_list
     include = include_list
     if add_noise:
-        composed = transforms.Compose([Cut(row_slice=row_slice,col_slice=col_slice), GaussianNoise(var=0.1), Rescale(scale), ChannelsFirst(), ToTensor()])
+        composed = transforms.Compose([Cut(row_slice=row_slice,col_slice=col_slice), GaussianNoise(var=add_noise), Rescale(scale), ChannelsFirst(), ToTensor()])
     else:
         composed = transforms.Compose(
             [Cut(row_slice=row_slice, col_slice=col_slice), Rescale(scale), ChannelsFirst(), ToTensor()])
